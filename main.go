a93fb0429ddcba072c092cc60eda9c6db5089064
BubbleFish <daforsastudia@gmail.com>
2024-10-05 17:51:51 +0300

    feat: Add CLI handling (--version, --help) from parsex lib

--- main.go
+++ main.go
@@ -1,7 +1,30 @@
 package main
 
+import (
+	"log/slog"
+	"os"
+
+	"github.com/bbfh-dev/parsex/parsex"
+)
+
 var Version string
 
-func main() {
+func Program(in parsex.Input, args ...string) error {
+	if in.Has("version") {
+		println("Tuxle server " + Version)
+		return nil
+	}
 
+	return nil
+}
+
+var CLI = parsex.New("example", Program, []parsex.Arg{
+	{Name: "version", Match: "--AUTO,-v", Desc: "print version and exit"},
+})
+
+func main() {
+	if err := CLI.FromArgs().Run(); err != nil {
+		slog.Error(err.Error())
+		os.Exit(1)
+	}
 }
